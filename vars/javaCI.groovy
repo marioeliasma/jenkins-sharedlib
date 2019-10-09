28f99f3f267e100c9adb147f67534a8cd501d817
Mario Montenegro <me14montenegro@hotmail.com>
2019-10-09 00:02:34 -0500

    Fixed sonar host

--- vars/javaCI.groovy
+++ vars/javaCI.groovy
@@ -8,7 +8,7 @@ def call() {
         stage("sonar") {
             pom = readMavenPom file: 'pom.xml'
             withCredentials([string(credentialsId: 'sonar', variable: 'secret')]) {
-                sh "${MAVEN_HOME}/bin/mvn sonar:sonar -Dsonar.projectKey=${pom.groupId}:${pom.artifactId} -Dsonar.login=${secret} -Dsonar.host.url=http://localhost:9000"
+                sh "${MAVEN_HOME}/bin/mvn sonar:sonar -Dsonar.projectKey=${pom.groupId}:${pom.artifactId} -Dsonar.login=${secret} -Dsonar.host.url=http://vagrant_jenkins_1:9000"
             }
         }
         stage("Test") { 
