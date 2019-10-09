9e1549317dc5f88d08f36ed456b30697279e122e
Mario Montenegro <me14montenegro@hotmail.com>
2019-10-09 09:51:44 -0500

    Added security

--- vars/javaCI.groovy
+++ vars/javaCI.groovy
@@ -5,7 +5,7 @@ def call() {
         def MAVEN_HOME = tool name: 'maven-3.6.2', type: 'maven'
         def DOCKER_HOME = tool name: "docker-17.09.1-ce", type: 'org.jenkinsci.plugins.docker.commons.tools.DockerTool'
         
-        stage("sonar") {
+        stage("Quality") {
             pom = readMavenPom file: 'pom.xml'
             withCredentials([string(credentialsId: 'sonar', variable: 'secret')]) {
                 sh "${MAVEN_HOME}/bin/mvn sonar:sonar -Dsonar.projectKey=${pom.groupId}:${pom.artifactId} -Dsonar.login=${secret} -Dsonar.host.url=http://172.16.30.117:9000"
@@ -28,6 +28,12 @@ def call() {
                 sh "${DOCKER_HOME}/bin/docker push $username/${pom.artifactId}:${pom.version}"
             }
         }
+
+        stage('Security') {
+          sh 'mvn org.owasp:dependency-check-maven:check -Dformat=XML -DautoUpdate=false'
+          step([$class: 'DependencyCheckPublisher', unstableTotalAll: '0'])
+        }
+
     }
  
 }
