9c3e2f1a80e649919fd0f84947bec7f94a42ee07
Mario Montenegro <me14montenegro@hotmail.com>
2019-10-08 22:58:49 -0500

    Changed it to scripted

--- vars/javaCI.groovy
+++ vars/javaCI.groovy
@@ -1,37 +1,23 @@
 def call() {
 
-pipeline {
+    node {
 
-    agent {
-        label 'master'
-    }
-
-    environment {
-        MAVEN_HOME = tool name: 'maven-3.6.2', type: 'maven'
-        DOCKER_HOME = tool name: 'docker-17.09.1-ce', type: 'org.jenkinsci.plugins.docker.commons.tools.DockerTool'
-    }
-    
-        stage("Test") {
-            steps {
-                checkout scm
-                sh "${MAVEN_HOME}/bin/mvn test"
-            }
+        def MAVEN_HOME = tool name: 'maven-3.6.2', type: 'maven'
+        def DOCKER_HOME = tool name: 'docker-17.09.1-ce', type: 'org.jenkinsci.plugins.docker.commons.tools.DockerTool'
+        
+        stage("Test") { 
+            checkout scm
+            sh "${MAVEN_HOME}/bin/mvn test"
         }
 
         stage ("Build") {
-            steps {
-                sh "${MAVEN_HOME}/bin/mvn clean package" 
-            }
+            sh "${MAVEN_HOME}/bin/mvn clean package" 
         }
 
         stage("Deploy to Docker hub") {
-            steps {
-                docker.build("${DOCKER_HOME}","consultec-test:v1", "${WORSPACE}")
-                docker.publish("${DOCKER_HOME}","consultec-test:v1")
-            }
+            docker.build("${DOCKER_HOME}","consultec-test:v1", "${WORSPACE}")
+            docker.publish("${DOCKER_HOME}","consultec-test:v1")
         }
-    
-    
-}
-    
+    }
+ 
 }
